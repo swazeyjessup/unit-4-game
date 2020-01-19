6bea439273ba5629f30b67eadd60e5cc87d4f2a6
Swazey Jessup <swazey_jessup@hotmail.com>
2020-01-18 23:07:19 -0700

    Added CCS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,6 +12,79 @@ $( document ).ready(function(){
    var num3 = Math.floor(Math.random() * 11 + 1);
    var num4 = Math.floor(Math.random() * 11 + 1);
    
+    var userTotal = 0;
+    var wins = 0;
+    var losses = 0; 
 
+    $("#numberWins").text(wins);
+    $("#numberLosses").text(losses);
 
+    //Resets the Game//
+    function reset(){
+        Random = Math.floor(Math.random() * 101 + 19);
+        $("#randomNumber").text(Random);
+        num1 = Math.floor(Math.random() * 11 +1);
+        num2 = Math.floor(Math.random() * 11 +1);
+        num3 = Math.floor(Math.random() * 11 +1);
+        num4 = Math.floor(Math.random() * 11 +1);
+        userTotal = 0;
+        $("#finalTotal").text(userTotal);
+    }
+
+    //Adds to the wins Total//
+    function winner(){
+        alert("YOU WON!!!");
+        wins++;
+        $("#numberWins").text(wins);
+        reset();
+    }
+
+    //Adds to the Losses Total//
+    function loser(){
+        alert ("YOU LOST!!!")
+        losses++;
+        $("#numberLosses").text(losses);
+        reset();
+    }
+
+    $("#red").on("click", function(){
+        userTotal = userTotal + num1;
+        console.log("New userTotal= "+ userTotal);
+        $("#finalTotal").text(userTotal);
+            if(userTotal === Random){
+                winner();
+            }else if ( userTotal > Random){
+                loser();
+            }
+    })
+    $("#green").on("click", function(){
+        userTotal = userTotal + num2;
+        console.log("New userTotal= "+ userTotal);
+        $("#finalTotal").text(userTotal);
+            if(userTotal === Random){
+                winner();
+            }else if ( userTotal > Random){
+                loser();
+            }
+    })
+    $("#pink").on("click", function(){
+        userTotal = userTotal + num3;
+        console.log("New userTotal= "+ userTotal);
+        $("#finalTotal").text(userTotal);
+            if(userTotal === Random){
+                winner();
+            }else if ( userTotal > Random){
+                loser();
+            }
+    })
+    $("#orange").on("click", function(){
+        userTotal = userTotal + num4;
+        console.log("New userTotal= "+ userTotal);
+        $("#finalTotal").text(userTotal);
+            if(userTotal === Random){
+                winner();
+            }else if ( userTotal > Random){
+                loser();
+            }
+    })
 })
