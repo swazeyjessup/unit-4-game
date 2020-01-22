9f3e58d93fb5d4e69eefa98fde8b5210f6fcdddb
Swazey Jessup <swazey_jessup@hotmail.com>
2020-01-21 21:27:25 -0700

    Fixxed win loss counter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -35,7 +35,7 @@ $( document ).ready(function(){
     function winner(){
         alert("YOU WON!!!");
         wins++;
-        $("#numberWins").text(losses);
+        $("#numberWins").text(wins);
         reset();
     }
 
